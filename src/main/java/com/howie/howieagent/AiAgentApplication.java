b19d31604da77bc2fa46b15d55a07ed84f94edbf
luckyren00 <lucky_ren00@163.com>
2025-07-16 11:18:34 +0800

    modify package name

--- src/main/java/com/howie/howieagent/AiAgentApplication.java
+++ src/main/java/com/howie/howieagent/AiAgentApplication.java
@@ -1,4 +1,4 @@
-package com.howie.agent;
+package com.howie.howieagent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/howie/howieagent/controller/HealthController.java
+++ src/main/java/com/howie/howieagent/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.howie.agent.controller;
+package com.howie.howieagent.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
