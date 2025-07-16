a67709ccedfb2775700b11bbc29758c5c71f4f05
luckyren00 <lucky_ren00@163.com>
2025-07-16 10:09:33 +0800

    项目初始化1

---
+++ src/main/java/com/howie/agent/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.howie.agent.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String healthCheck(){
+        return "ok";
+    }
+
+}
